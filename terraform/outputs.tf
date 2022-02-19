5290fade90498607f36f1c739f0f3e34615147f5
AngelVlc <angel.ballesteros@domestika.org>
2022-02-19 20:27:23 +0100

    Sets outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,23 @@
-output "function_name" {
-  description = "Name of the Lambda function."
+output "invokation_url" {
+  description = "Invokation url"
 
-  value = aws_lambda_function.hello_world_lambda.function_name
+  value = aws_apigatewayv2_stage.production.invoke_url
+}
+
+output "integration_route" {
+  description = "Integration route"
+
+  value = aws_apigatewayv2_route.hello_world_api_gateway_route.route_key
+}
+
+output "log_group_api_gateway" {
+  description = "Api Gateway log group"
+
+  value = aws_cloudwatch_log_group.hello_world_api_gateway_log_group.name
+}
+
+output "log_group_lambda" {
+  description = "Lambda log group"
+
+  value = aws_cloudwatch_log_group.hello_world_lambda_log_group.name
 }
